a2628784f1a6ef3eacccead7a98c9dd172d6af8e
dvrpcsh <xoxoxmin1268@naver.com>
2023-09-20 10:13:47 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,11 +2,36 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://shchoi:star15321@cluster0.pw7lnn6.mongodb.net/?retryWrites=true&w=majority'
-).then(() => console.log('MongoDB Connected...'))
- .catch(err => console.log(err))
+const { User } = require("./models/User");
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
+
+
+mongoose.connect(config.mongoURI)
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req,res) => {
+    //회원가입 할 때 필요한 정보를 client에서 가져와 db insert
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.status(200).json({
+            success: true
+        })
+    }).catch((err) => {
+        res.json({ success: false,err})
+    })
 })
 
 app.listen(port, () => {
